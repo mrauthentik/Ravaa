535b2f85f09f127e03d4b9c08429d5dc7bc06133
mrauthentik <umokeuchenna2020@gmail.com>
2025-02-03 17:05:52 +0100

    add Core component with styling and core values section

--- zma/src/Core.jsx
+++ zma/src/Core.jsx
@@ -1,13 +1,40 @@
-
+import img1 from './assets/icons (7).png'
+import img2 from './assets/icons (6).png'
+import img3 from './assets/icons (5).png'   
+import img4 from './assets/icons (4).png'
 
 const Core = () => {
   return (
     <div className="core">
        <h1>Core Values</h1>
-       <p>We strive for the highest standards in every project, 
+       <p>We strive for the highest standards in every project, <br />
        ensuring quality and impact.</p>
          <div className="core-flex">
-            
+            <div className="flex">
+                <img src={img1} alt="" />
+                <h2>Integrity</h2>
+                <p>We always do what is right and 
+                keep our words.</p>
+            </div>
+            <div className="flex">
+                <img src={img2} alt="" />
+                <h2>Compasion</h2>
+                <p>we have keen interest in our
+                customers’s need and want.</p>
+            </div>
+            <div className="flex">
+                <img src={img3} alt="" />
+                <h2>Excelence</h2>
+                <p>We believe that anything worth
+                doing, is worth doing well.</p>
+            </div>
+            <div className="flex">
+                <img src={img4} alt="" />
+                <h2>Deligience</h2>
+                <p>Constant and ernest effort
+                    to accomplish task for our
+                    client with precision</p>
+            </div>
          </div>
     </div>
   )
