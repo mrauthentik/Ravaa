19bcda8e20bd93afbd4b8fd32a98916a6779d400
mrauthentik <umokeuchenna2020@gmail.com>
2025-02-03 16:36:02 +0100

    add descriptive paragraph to Core component to highlight project standards

--- zma/src/Core.jsx
+++ zma/src/Core.jsx
@@ -4,8 +4,11 @@ const Core = () => {
   return (
     <div className="core">
        <h1>Core Values</h1>
-       <p></p>
-
+       <p>We strive for the highest standards in every project, 
+       ensuring quality and impact.</p>
+         <div className="core-flex">
+            
+         </div>
     </div>
   )
 }
