11ae970999cab817b93f20fa6922e38c2270e008
mrauthentik <umokeuchenna2020@gmail.com>
2025-02-03 14:37:56 +0100

    reduce paragraph font size and improve text formatting in Hero component for better readability

--- zma/src/Hero.jsx
+++ zma/src/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
     <div className="hero">
       <div className="hero-text">
       <h1>Bridging the gap between local potential and global opportunities</h1>
-      <p>As a dynamic development consulting firm, we specialize in empowering individuals, businesses, and communities to thrive in an increasingly interconnected world.</p>
+      <p>As a dynamic development consulting firm, we specialize in empowering individuals, <br /> businesses, and communities to thrive in an increasingly interconnected world.</p>
       </div>
       <div className="hero-btn">
         <button className="hero-btn">Our services</button>
