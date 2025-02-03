ee7fa7e015ceea6b3c0ca730f9efc96f63aa88a2
mrauthentik <umokeuchenna2020@gmail.com>
2025-02-03 14:11:15 +0100

    add descriptive subtitle to Hero component for enhanced context

--- zma/src/Hero.jsx
+++ zma/src/Hero.jsx
@@ -5,6 +5,8 @@ const Hero = () => {
       <h1>Bridging the gap between 
 local potential and global 
 opportunities</h1>
+<h5>As a dynamic development consulting firm, we specialize in empowering individuals, 
+businesses, and communities to thrive in an increasingly interconnected world.</h5>
     </div>
   )
 }
