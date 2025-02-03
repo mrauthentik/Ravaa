c60b63e8c07800c7db2f3b0270ac6ffe7053b750
mrauthentik <umokeuchenna2020@gmail.com>
2025-02-03 15:12:05 +0100

    refactor button styles in Hero component and add fill effect on hover for enhanced interactivity

--- zma/src/Hero.jsx
+++ zma/src/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
       <p>As a dynamic development consulting firm, we specialize in empowering individuals, <br /> businesses, and communities to thrive in an increasingly interconnected world.</p>
       </div>
       <div className="hero-btn">
-        <button className="hero-btn">Our services</button>
+        <button className="hero---btn">Our services</button>
         <button className="hero--btn">Get in touch</button>
       </div>
     </div>
