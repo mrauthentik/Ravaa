4cd3f46e13423cde5fb8c108cf9542a43487a9d1
mrauthentik <umokeuchenna2020@gmail.com>
2025-03-03 13:18:30 +0000

    add @formspree/react and react-toastify dependencies to package.json and package-lock.json

--- zma/src/Contact.jsx
+++ zma/src/Contact.jsx
@@ -1,10 +1,12 @@
 import { motion } from "framer-motion";
+import {useForm, ValidationError} from '@formspree/react';
 
 const Contact = () => {
   const fadeInUp = {
     hidden: { opacity: 0, y: 50 },
     visible: { opacity: 1, y: 0, transition: { duration: 0.6, ease: "easeOut" } }
   };
+  const [state, handleSubmit] = useForm("mgvogrog");
 
   return (
     <motion.div 
