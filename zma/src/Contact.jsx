fce5cb076ef97a3b94aee72918c862c7c65629a1
mrauthentik <umokeuchenna2020@gmail.com>
2025-03-04 18:27:06 +0100

    refactor Contact component to display success message and enhance form validation with error handling

--- zma/src/Contact.jsx
+++ zma/src/Contact.jsx
@@ -21,11 +21,62 @@ const Contact = () => {
     })
   }
 
-  if (state.succeeded) {
-    return success();
-  }
+  
   return (
-    <motion.div 
+    <>
+    {state.succeeded ? (
+      
+    <div>  {success}
+        <motion.div 
+      className="contact"
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: false, amount: 0.2 }}
+    >
+      <motion.div className="contact-block" variants={fadeInUp}>
+        <motion.div className="contact-text" variants={fadeInUp}>
+          <h1>Get in touch with us</h1>
+          <p>
+           
+            Ready to collaborate and drive sustainable development? <br />
+            <br /> Contact us today to explore how our expertise can
+            <br /> support your organization&apos;s goals.
+          </p>
+        </motion.div>
+
+        <motion.form action="https://formspree.io/f/mgvogrog" onSubmit={handleSubmit} variants={fadeInUp} className="contact-form">
+          <input type="text" placeholder="Full name" name="name" id="name" />
+           <ValidationError
+            prefix="Name"
+            field="name"
+            errors={state.errors} />
+
+          <input type="email" placeholder="Email address" id="email" name="email" />
+          <ValidationError
+            prefix="Email"
+            field="email"
+            errors={state.errors} />
+
+          <input type="tel" placeholder="Phone number" id="number" name="number" />
+          <ValidationError
+            prefix="Number"
+            field="number"
+            errors={state.errors} />
+
+          <textarea cols="50" rows="4" placeholder="Message"></textarea>
+          <ValidationError
+            prefix="Message"
+            field="mesaage"
+            errors={state.errors} />
+
+          <button type="submit" disabled={state.submitting} className="form-btn">Submit inquiry</button>
+        </motion.form>
+      </motion.div>
+    </motion.div>
+    </div>
+    
+    ) : (
+      <motion.div 
       className="contact"
       initial="hidden"
       whileInView="visible"
@@ -70,6 +121,10 @@ const Contact = () => {
         </motion.form>
       </motion.div>
     </motion.div>
+    )
+
+    }
+   </>
   );
 };
 
